ceb280b1bc1bb423873d6a636a89e4a3ee8475b8
Molly Huang <molly.h@gmail.com>
2021-02-20 13:08:39 +0800

    Lesson 3 - end here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
       </div>
     </div>
   );
