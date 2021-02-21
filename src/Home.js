73b7ae6cdad54cb3e40ae4498241550d98837908
Molly Huang <molly.h@gmail.com>
2021-02-21 15:53:06 +0800

    Lesson8-end - Using State

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
+  //let name = 'mario';
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
 
-  const handleClick = (e) => {
-    console.log("hello, there...", e);
-  }
-
-  const handleClickAgain = (name, e) => {
-    console.log("hello, " + name, e.target);
+  const handleClick = () => {
+    setName('luigi');
+    setAge(34);
+    console.log(name, age);
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name } is { age } years old</p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain("Molly", e)}>Click me Again</button>
     </div>    
   );
 }
