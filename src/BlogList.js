af232f00d653b662b76eba118819c7a746ec732d
Molly Huang <molly.h@gmail.com>
2021-02-21 19:21:57 +0800

    #17 - Fetching Data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,5 @@
 
-const BlogList = ({blogs, title, handleDelete}) => {
-// const BlogList = (props) => {
-//   const blogs = props.blogs;
-//   const title = props.title;
-//   console.log(blogs, title);
-
+const BlogList = ({blogs, title}) => {
   return (
     <div className="blog-list">
       <h2>{ title }</h2>
@@ -12,7 +7,6 @@ const BlogList = ({blogs, title, handleDelete}) => {
         <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
-          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -2,32 +2,25 @@ import { useState, useEffect } from 'react';
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null);
 
-  const [name, setName] = useState('mario');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
+  //useEffect(async () => {   // this could wait and sync
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(name);
+    fetch ('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        setBlogs(data);
+      })
+  }, []);   // This would run once on the first rendering
   // });   // This would run after every single rendering
   // }, []);   // This would run once on the first rendering
-  }, [name]);   // This would run once on the first rendering
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>Change name</button>
-      { name }
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 }
