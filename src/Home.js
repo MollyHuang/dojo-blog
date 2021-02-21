6fd38767e4eb89ce28eb313495ac500fb9b1db47
Molly Huang <molly.h@gmail.com>
2021-02-21 15:31:40 +0800

    Lesson7-end-click-parameters

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log("hello, there...", e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello, " + name, e.target);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("Molly", e)}>Click me Again</button>
     </div>    
   );
 }
